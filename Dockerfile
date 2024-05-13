90ced2bd53a9e1fa413b8cb2c9f952485183b484
kidkender <kidkender@gmail.com>
2024-05-13 22:58:23 +0700

    update deploy gunicorn & nginx

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ FROM python:3.11.5
 ENV PYTHONDONTWRITEBYTECODE 1
 ENV PYTHONUNBUFFERED 1
 
+RUN mkdir -p /app/staticfiles
+COPY static /app/staticfiles  
 WORKDIR /app 
 
 COPY requirements.txt /app/ 
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,20 +2,24 @@
 if [ "$DATABASE" = "mysql"]
 then
     echo "Waiting for mysql"
-    while ! nc -z$SQL_HOST $SQL_PORT; do
+    while ! nc -z $SQL_HOST $SQL_PORT; do
         sleep 0.1
     done 
         echo "Start mysql successfully"
+
 echo "Make new migrations"
-python manage.py makemigrations
+python3 manage.py makemigrations
 
 echo "Apply database migrations"
-python manage.py migrate
+python3 manage.py migrate --noinput
 
 echo "Apply import data from excel"
-python manage.py import_data suburbs_3.xlsx
+python3 manage.py import_data suburbs_10.xlsx
+
+echo "Collect static files"
+python3 manage.py collectstatic --noinput --ignore static
 
 echo "Start server" 
-python manage.py runserver
+python manage.py runserver --bind 0.0.0.0:8000"
 
 exec "$@"
